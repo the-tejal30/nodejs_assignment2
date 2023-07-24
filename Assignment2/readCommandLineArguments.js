32159ac94b801d9af6de733a1010f722df319bbd
Tejal Satish Wani <127750682+tedi3001@users.noreply.github.com>
2023-07-24 22:14:24 +0530

    Add files via upload

---
+++ Assignment2/readCommandLineArguments.js
@@ -0,0 +1,3 @@
+// readCommandLineArguments.js
+const name = process.argv[2];
+console.log(`Hello ${name}`);
---
+++ Assignment2/readEnvVariable.js
@@ -0,0 +1,6 @@
+// readEnvVariable.js
+const process = require('process');
+
+const name = process.env.USERNAME;
+console.log(`Hello ${name}`);
+
